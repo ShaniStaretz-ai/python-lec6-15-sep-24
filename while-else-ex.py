db301fa5b0dbbd28480779ce6ea454e16b2181c4
Shani Staretz <shani.s@firstpoint-mg.com>
2024-09-15 18:23:59 +0300

    fixed when the user cant num=0 on the first try

--- while-else-ex.py
+++ while-else-ex.py
@@ -6,7 +6,7 @@ while num != LUCK_NUMBER:
     num: int = int(input("enter number:"));
     tries += 1;
     if num == BREAK_NUMBER:
-        print("num is 0");
+        print(f"you failed!, you guessed the break number: {num} in {tries} tries")
         break;
 else:
     print(f"well done!, you guessed the number {num} in {tries} tries")
