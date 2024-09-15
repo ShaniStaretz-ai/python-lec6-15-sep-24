ad35d21c4e489a006a2d03b87fcbe2ad300b0563
Shani Staretz <shani.s@firstpoint-mg.com>
2024-09-15 18:40:35 +0300

    loop else in for loop

--- while-else-ex.py
+++ while-else-ex.py
@@ -1,12 +1,23 @@
-tries = 0;
-num = -1;
-LUCK_NUMBER: int = 777;
-BREAK_NUMBER = 0;
-while num != LUCK_NUMBER:
+# tries = 0;
+# num = -1;
+# LUCK_NUMBER: int = 777;
+# BREAK_NUMBER = 0;
+# while num != LUCK_NUMBER:
+#     num: int = int(input("enter number:"));
+#     tries += 1;
+#     if num == BREAK_NUMBER:
+#         print(f"you failed!, you guessed the break number: {num} in {tries} tries")
+#         break;
+# else:
+#     print(f"well done!, you guessed the number {num} in {tries} tries")
+
+
+N:int=3;sum_n:int=0;
+for _ in range(N):
     num: int = int(input("enter number:"));
-    tries += 1;
-    if num == BREAK_NUMBER:
-        print(f"you failed!, you guessed the break number: {num} in {tries} tries")
+    if not 0<num<100:
+        print("invalid value:",num)
         break;
+    sum_n+=num;
 else:
-    print(f"well done!, you guessed the number {num} in {tries} tries")
+    print(f"avg={sum_n//N}")
