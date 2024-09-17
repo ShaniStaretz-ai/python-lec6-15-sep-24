23f2e2c56f8462d2497cd1447b53fb05ce8e4c0d
ShaniStaretz-ai <shani.staretz@gmail.com>
2024-09-17 14:41:26 +0300

    Update hw6-2.5.py

--- hw/hw6-2.5.py
+++ hw/hw6-2.5.py
@@ -3,7 +3,7 @@ sum_temperature: int = 0;
 for _ in range(5):
     temperature = int(input("enter temperature:"));
     while temperature < -50 or temperature > 45:
-        temperature = int(input("try enter a valid temp:"));
+        temperature = int(input("try enter a valid temperature:"));
     else:
         sum_temperature += temperature;
 print(f"the average of all temp is {sum_temperature // 5}");
